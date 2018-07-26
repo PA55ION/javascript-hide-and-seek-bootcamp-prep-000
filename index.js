dea37b3a7b6e85bb8c3a08d70c8a3886f0dd55a7
Mai Campbell <jirallc@gmail.com>
2018-07-26 13:10:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+   return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('.target')
+
+}
+function increaseRankBy(n) {
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+
+}
