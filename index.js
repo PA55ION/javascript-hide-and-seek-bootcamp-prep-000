7ce154c241365a31c1d6670572ad46060dc7f93b
Mai Campbell <jirallc@gmail.com>
2018-07-26 13:20:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+   return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('.target')
+
+}
+function increaseRankBy(n) {
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var child = document.getElementById('grand-node').querySelectorAll('div');
+
+  var test;
+  for(let i = 0; i < child.length - 1; i++) {
+    test = child[i].querySelector('div')
+  }
+  return test;
+}
